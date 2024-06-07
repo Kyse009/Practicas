33216b24bf0d176e2f011e92f19bab712dfccf94
kysehu <maniacomunicacion1@gmail.com>
2024-06-07 16:16:05 -0400

    Add files via upload

---
+++ conversor.js
@@ -0,0 +1,14 @@
+let Ingrese = Number;
+let ingresados = '';
+
+while (ingresarCantidad != 0) {
+    ingresados += ingresarCantidad +"\n";
+}
+
+function conversor(ingresarCantidad, elegirMoneda) {
+    let dolar = 240;
+    let euro = 260;
+
+    if (Ingrese === dolar) {
+    }
+}
